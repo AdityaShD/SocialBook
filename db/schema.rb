cadbc0dad92c8bc2038650d2580291e7f44feee0
Addy <adityasharma965@gmail.com>
2018-08-24 14:35:27 +0530

    field added in about section

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180822184353) do
+ActiveRecord::Schema.define(version: 20180824074944) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content", null: false
@@ -18,6 +18,23 @@ ActiveRecord::Schema.define(version: 20180822184353) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "controllers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_controllers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_controllers_on_reset_password_token", unique: true
   end
 
   create_table "likes", force: :cascade do |t|
